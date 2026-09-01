771b36942a30c827320622b8fc0a4726a60d3502
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2026-09-01 13:58:57 +0800

    update

--- devices/airoha_an7581/diy.sh
+++ devices/airoha_an7581/diy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-curl -sL https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/24883.patch | git apply -v
+shopt -s extglob
+
+#curl -sL https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/24883.patch | git apply -v
+
+rm -rf target/linux/airoha/!(patches-6.12) package/boot
+
+git_clone_path main https://github.com/openwrt/openwrt target/linux/airoha package/boot
+
+wget -N https://github.com/openwrt/openwrt/raw/refs/heads/openwrt-25.12/target/linux/airoha/an7581/config-6.12 -P target/linux/airoha/an7581/
+
+sed -i "s/KERNEL_PATCHVER:=6.18/KERNEL_PATCHVER:=6.12/" target/linux/airoha/Makefile
